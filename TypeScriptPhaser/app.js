a5685aeb0e9b6152d0f33bbdd65cec3bc9c4c1ec
Lithry <facundosargiotti@gmail.com>
2017-07-13 02:25:57 -0300

    OK

--- TypeScriptPhaser/app.js
+++ TypeScriptPhaser/app.js
@@ -9,21 +9,20 @@ var Game;
         }
         TypeScriptPhaser.prototype.preload = function () {
             this.game.load.image('Background', 'Assets/Background.jpg');
-            this.game.load.image('Platforms', 'Assets/Platform.png');
+            this.game.load.image('Platforms', 'Assets/Plat.png');
             this.game.load.image('Star', 'Assets/Star.png');
             this.game.load.spritesheet('dude', 'Assets/dude.png', 32, 48);
         };
         TypeScriptPhaser.prototype.create = function () {
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
             this.game.add.sprite(-10, -10, 'Background');
+            this.restartButton = this.game.input.keyboard.addKey(Phaser.Keyboard.R);
             // Player
             this.player = this.game.add.sprite(50, this.game.height / 2, 'Platforms');
             this.game.physics.arcade.enable(this.player, true);
             this.player.enableBody = true;
             this.player.body.immovable = true;
             this.player.anchor.setTo(0.5, 0.5);
-            this.player.scale.setTo(0.2, 0.2);
-            this.player.angle = 90;
             this.player.body.allowGravity = false;
             // Enemy
             this.enemy = this.game.add.sprite(this.game.width - 50, this.game.height / 2, 'Platforms');
@@ -31,8 +30,6 @@ var Game;
             this.enemy.enableBody = true;
             this.enemy.body.immovable = true;
             this.enemy.anchor.setTo(0.5, 0.5);
-            this.enemy.scale.setTo(0.2, 0.2);
-            this.enemy.angle = 90;
             this.enemy.body.allowGravity = false;
             // Boll
             this.boll = this.game.add.sprite(600, this.game.height / 2, 'Star');
@@ -40,50 +37,103 @@ var Game;
             this.player.enableBody = true;
             this.player.body.collideWorldBounds = true;
             this.player.body.allowGravity = false;
-            this.boll.body.velocity.x = 175;
-            this.boll.body.velocity.y = 160;
+            this.boll.body.velocity.x = -175;
+            this.boll.body.velocity.y = 0;
+            this.boolUp = true;
+            var bar = this.game.add.graphics();
+            bar.beginFill(0xFF00FF, 0.2);
+            bar.drawRect(0, 0, this.game.width, 100);
+            var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+            this.text = this.game.add.text(this.game.width / 2 - 20, 35, "0 - 0", style);
+            this.text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+            this.restartText = this.game.add.text(20, 20, "R to Restart", style);
+            this.restartText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+            this.scorePlayer = 0;
+            this.scoreEnemy = 0;
             this.cursors = this.game.input.keyboard.createCursorKeys();
         };
         TypeScriptPhaser.prototype.update = function () {
             // collision
-            this.game.physics.arcade.collide(this.boll, this.enemy, this.OnCollisionBoll, null, this.game);
-            this.game.physics.arcade.overlap(this.boll, this.player, this.OnCollisionBoll, null, this.game);
+            this.game.physics.arcade.collide(this.boll, this.enemy);
+            this.game.physics.arcade.collide(this.boll, this.player);
+            // Score
+            this.text.text = this.scorePlayer + ' - ' + this.scoreEnemy;
+            // Restar
+            if (this.restartButton.justPressed()) {
+                this.scorePlayer = 0;
+                this.scoreEnemy = 0;
+                this.boll.y = this.game.height / 2;
+                this.boll.x = this.game.width / 2;
+                this.player.y = this.game.height / 2;
+                this.enemy.y = this.game.height / 2;
+                this.boll.body.velocity.x = -175;
+                this.boll.body.velocity.y = 0;
+                this.boolUp = true;
+            }
             // Player
             this.player.body.velocity.y = 0;
-            if (this.cursors.up.isDown) {
-                this.player.body.velocity.y = -250;
+            if (this.cursors.up.isDown && this.player.y > 150) {
+                this.player.body.velocity.y = -170;
             }
-            else if (this.cursors.down.isDown) {
-                this.player.body.velocity.y = 250;
+            else if (this.cursors.down.isDown && this.player.x < this.game.height - 150) {
+                this.player.body.velocity.y = 170;
             }
             // Enemy
             this.enemy.body.velocity.y = 0;
-            if (this.enemy.y - 10 > this.boll.y) {
-                this.enemy.body.velocity.y = -150;
+            if (this.enemy.y - 10 > this.boll.y && this.enemy.y > 150) {
+                this.enemy.body.velocity.y = -170;
             }
-            else if (this.enemy.y + 10 < this.boll.y) {
-                this.enemy.body.velocity.y = 150;
+            else if (this.enemy.y + 10 < this.boll.y && this.enemy.y < this.game.height - 50) {
+                this.enemy.body.velocity.y = 170;
             }
-            // Boll
-            if (this.boll.y < 20) {
-                this.boll.body.velocity.y = 175;
+            // Boll Limits
+            if (this.boll.body.velocity.y < 0) {
+                this.boolUp = true;
             }
-            else if (this.boll.y > this.game.height - 20) {
-                this.boll.body.velocity.y = -175;
+            else {
+                this.boolUp = false;
             }
+            if (this.boll.y < 100 && this.boolUp) {
+                this.boll.body.velocity.y = this.boll.body.velocity.y * (-1);
+            }
+            else if (this.boll.y > this.game.height - 20 && !this.boolUp) {
+                this.boll.body.velocity.y = -(this.boll.body.velocity.y);
+            }
+            // Boll Collision E&P
             if (this.boll.body.touching.right) {
                 this.boll.body.velocity.x = -175;
+                if (this.boll.y > this.enemy.y) {
+                    this.boll.body.velocity.y = (Phaser.Math.difference(this.enemy.y, this.boll.y) * 5);
+                }
+                else {
+                    this.boll.body.velocity.y = -(Phaser.Math.difference(this.enemy.y, this.boll.y) * 5);
+                }
             }
             else if (this.boll.body.touching.left) {
                 this.boll.body.velocity.x = 175;
+                if (this.boll.y > this.player.y) {
+                    this.boll.body.velocity.y = (Phaser.Math.difference(this.player.y, this.boll.y) * 4);
+                }
+                else {
+                    this.boll.body.velocity.y = -(Phaser.Math.difference(this.player.y, this.boll.y) * 4);
+                }
             }
-        };
-        TypeScriptPhaser.prototype.OnCollisionBoll = function () {
-            if (this.boll.body.velocity.x > 0) {
-                this.boll.body.velocity.x = -175;
-            }
-            else if (this.boll.body.velocity.x < 0) {
-                this.boll.body.velocity.x = 175;
+            // Gool
+            if (this.boll.x > this.enemy.x || this.boll.x < this.player.x) {
+                if (this.boll.x > this.enemy.x) {
+                    this.scorePlayer += 1;
+                    this.boll.body.velocity.x = -175;
+                    this.enemy.y = this.game.height / 2;
+                }
+                else {
+                    this.scoreEnemy += 1;
+                    this.boll.body.velocity.x = 175;
+                    this.enemy.y = this.game.height / 2 + 8;
+                }
+                this.boll.body.velocity.y = 0;
+                this.boll.y = this.game.height / 2;
+                this.boll.x = this.game.width / 2;
+                this.player.y = this.game.height / 2;
             }
         };
         return TypeScriptPhaser;
--- TypeScriptPhaser/app.ts
+++ TypeScriptPhaser/app.ts
@@ -6,7 +6,13 @@ module Game {
         player;
         enemy;
         boll;
+        boolUp;
         cursors;
+        text;
+        scorePlayer;
+        scoreEnemy;
+        restartButton;
+        restartText;
 
         constructor() {
             this.game = new Phaser.Game(1280, 720, Phaser.AUTO, 'content', {
@@ -16,7 +22,7 @@ module Game {
 
         preload() {
             this.game.load.image('Background', 'Assets/Background.jpg');
-            this.game.load.image('Platforms', 'Assets/Platform.png');
+            this.game.load.image('Platforms', 'Assets/Plat.png');
             this.game.load.image('Star', 'Assets/Star.png');
             this.game.load.spritesheet('dude', 'Assets/dude.png', 32, 48);
         }
@@ -24,6 +30,7 @@ module Game {
         create() {
             this.game.physics.startSystem(Phaser.Physics.ARCADE);
             this.game.add.sprite(-10, -10, 'Background');
+            this.restartButton = this.game.input.keyboard.addKey(Phaser.Keyboard.R);
 
             // Player
             this.player = this.game.add.sprite(50, this.game.height / 2, 'Platforms');
@@ -31,8 +38,6 @@ module Game {
             this.player.enableBody = true;
             this.player.body.immovable = true;
             this.player.anchor.setTo(0.5, 0.5);
-            this.player.scale.setTo(0.2, 0.2);
-            this.player.angle = 90;
             this.player.body.allowGravity = false;
             
             // Enemy
@@ -41,8 +46,6 @@ module Game {
             this.enemy.enableBody = true;
             this.enemy.body.immovable = true;
             this.enemy.anchor.setTo(0.5, 0.5);
-            this.enemy.scale.setTo(0.2, 0.2);
-            this.enemy.angle = 90;
             this.enemy.body.allowGravity = false;
 
             // Boll
@@ -51,58 +54,116 @@ module Game {
             this.player.enableBody = true;
             this.player.body.collideWorldBounds = true;
             this.player.body.allowGravity = false;
-            this.boll.body.velocity.x = 175;
-            this.boll.body.velocity.y = 160;
+            this.boll.body.velocity.x = -175;
+            this.boll.body.velocity.y = 0;
+            this.boolUp = true;
+
+            var bar = this.game.add.graphics();
+            bar.beginFill(0xFF00FF, 0.2);
+            bar.drawRect(0, 0, this.game.width, 100);
+
+            var style = { font: "bold 32px Arial", fill: "#fff", boundsAlignH: "center", boundsAlignV: "middle" };
+            this.text = this.game.add.text(this.game.width / 2 - 20, 35, "0 - 0", style);
+            this.text.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+            this.restartText = this.game.add.text(20, 20, "R to Restart", style);
+            this.restartText.setShadow(3, 3, 'rgba(0,0,0,0.5)', 2);
+
+            this.scorePlayer = 0;
+            this.scoreEnemy = 0;
 
             this.cursors = this.game.input.keyboard.createCursorKeys();
         }
 
         update() {
             // collision
-            this.game.physics.arcade.collide(this.boll, this.enemy, this.OnCollisionBoll, null, this.game);
-            this.game.physics.arcade.overlap(this.boll, this.player, this.OnCollisionBoll, null, this.game);
+            this.game.physics.arcade.collide(this.boll, this.enemy);
+            this.game.physics.arcade.collide(this.boll, this.player);
+
+            // Score
+            this.text.text = this.scorePlayer + ' - ' + this.scoreEnemy;
+
+            // Restar
+            if (this.restartButton.justPressed()) {
+                this.scorePlayer = 0;
+                this.scoreEnemy = 0;
+                this.boll.y = this.game.height / 2;
+                this.boll.x = this.game.width / 2;
+                this.player.y = this.game.height / 2;
+                this.enemy.y = this.game.height / 2;
+                this.boll.body.velocity.x = -175;
+                this.boll.body.velocity.y = 0;
+                this.boolUp = true;
+            }
+
 
             // Player
             this.player.body.velocity.y = 0;
-            if (this.cursors.up.isDown) {
-                this.player.body.velocity.y = -250;
+            if (this.cursors.up.isDown && this.player.y > 150) {
+                this.player.body.velocity.y = -170;
             }
-            else if (this.cursors.down.isDown) {
-                this.player.body.velocity.y = 250;
+            else if (this.cursors.down.isDown && this.player.x < this.game.height - 150) {
+                this.player.body.velocity.y = 170;
             }
 
             // Enemy
             this.enemy.body.velocity.y = 0;
-            if (this.enemy.y - 10 > this.boll.y) {
-                this.enemy.body.velocity.y = -150;
+            if (this.enemy.y - 10 > this.boll.y && this.enemy.y > 150) {
+                this.enemy.body.velocity.y = -170;
             }
-            else if (this.enemy.y + 10 < this.boll.y) {
-                this.enemy.body.velocity.y = 150;
-            }
-
-            // Boll
-            if (this.boll.y < 20) {
-                this.boll.body.velocity.y = 175;
-            }
-            else if (this.boll.y > this.game.height - 20) {
-                this.boll.body.velocity.y = -175;
+            else if (this.enemy.y + 10 < this.boll.y && this.enemy.y < this.game.height - 50) {
+                this.enemy.body.velocity.y = 170;
             }
 
-            if (this.boll.body.touching.right) {
+            // Boll Limits
+            if (this.boll.body.velocity.y < 0) {
+                this.boolUp = true;
+            }
+            else {
+                this.boolUp = false;
+            }
+            if (this.boll.y < 100 && this.boolUp) {
+                this.boll.body.velocity.y = this.boll.body.velocity.y * (-1);
+            }
+            else if (this.boll.y > this.game.height - 20 && !this.boolUp) {
+                this.boll.body.velocity.y = -(this.boll.body.velocity.y);
+            }
+            // Boll Collision E&P
+            if (this.boll.body.touching.right) { //E
                 this.boll.body.velocity.x = -175;
+                if (this.boll.y > this.enemy.y) {
+                    this.boll.body.velocity.y = (Phaser.Math.difference(this.enemy.y, this.boll.y) * 5);
+                }
+                else {
+                    this.boll.body.velocity.y = -(Phaser.Math.difference(this.enemy.y, this.boll.y) * 5);
+                }
             }
 
-            else if (this.boll.body.touching.left) {
+            else if (this.boll.body.touching.left) { //P
                 this.boll.body.velocity.x = 175;
+                if (this.boll.y > this.player.y) {
+                    this.boll.body.velocity.y = (Phaser.Math.difference(this.player.y, this.boll.y) * 4);
+                }
+                else {
+                    this.boll.body.velocity.y = -(Phaser.Math.difference(this.player.y, this.boll.y) * 4);
+                }
             }
-        }
-
-        OnCollisionBoll() {
-            if (this.boll.body.velocity.x > 0) {
-                this.boll.body.velocity.x = -175;
-            }
-            else if (this.boll.body.velocity.x < 0) {
-                this.boll.body.velocity.x = 175;
+            // Gool
+            if (this.boll.x > this.enemy.x || this.boll.x < this.player.x) {
+                if (this.boll.x > this.enemy.x) {
+                    this.scorePlayer += 1;
+                    this.boll.body.velocity.x = -175;
+                    this.enemy.y = this.game.height / 2;
+                }
+                else {
+                    this.scoreEnemy += 1;
+                    this.boll.body.velocity.x = 175;
+                    this.enemy.y = this.game.height / 2 + 8;
+                }
+                this.boll.body.velocity.y = 0;
+                this.boll.y = this.game.height / 2;
+                this.boll.x = this.game.width / 2;
+                this.player.y = this.game.height / 2;
+                
             }
         }
     }
